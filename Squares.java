eb9d082f03a5bda41b63ac8fe9fa017cc097a47c
Vipul Sankhe <vipulx44@gmail.com>
2017-09-07 20:19:28 +0530

    Added java solution

---
+++ Squares.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Squares{
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        while(true){
+        int sum=0;
+        int m=sc.nextInt();
+        if(m==0)
+        {
+            break;
+        }
+        sum =m*(m+1)*(2*m+1)/6;
+        System.out.println(sum);
+        
+        8:18 PM 07-Sep-17}
+    }
+    
+}
